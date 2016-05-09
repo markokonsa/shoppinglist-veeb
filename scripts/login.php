9bb5e274c47ec8492c48e526a68a5b1fdb70fbe3
markokonsafob <marko.konsa@fob-solutions.com>
2016-05-09 17:17:18 +0300

    sha1 encryption to password

--- scripts/login.php
+++ scripts/login.php
@@ -6,7 +6,7 @@ connect_db();
 
 $username = mysqli_real_escape_string($connection, $_POST['username']);
 $password = mysqli_real_escape_string($connection, $_POST['password']);
-$result = mysqli_query($connection ,"SELECT id, username FROM mkonsa_ostukorv_kasutaja WHERE username='$username' AND password='$password' LIMIT 1") or die(mysqli_error($connection));
+$result = mysqli_query($connection ,"SELECT id, username FROM mkonsa_ostukorv_kasutaja WHERE username='$username' AND password=SHA1('$password') LIMIT 1") or die(mysqli_error($connection));
 $row    = mysqli_fetch_array($result);
 $count = mysqli_num_rows($result);
 if($count == 1) {
--- scripts/register.php
+++ scripts/register.php
@@ -13,8 +13,8 @@ if ($password == $password2) {
     $checkExistance = mysqli_query($connection, "SELECT username FROM mkonsa_ostukorv_kasutaja WHERE username='$username' LIMIT 1");
     if (mysqli_num_rows($checkExistance) == 0) {
 
-        $result = mysqli_query($connection, "INSERT INTO mkonsa_ostukorv_kasutaja (username, password) VALUES ('$username', '$password')") or die(mysqli_error($connection));
-        $result = mysqli_query($connection ,"SELECT id, username FROM mkonsa_ostukorv_kasutaja WHERE username='$username' AND password='$password' LIMIT 1") or die(mysqli_error($connection));
+        $result = mysqli_query($connection, "INSERT INTO mkonsa_ostukorv_kasutaja (username, password) VALUES ('$username', SHA1('$password'))") or die(mysqli_error($connection));
+        $result = mysqli_query($connection ,"SELECT id, username FROM mkonsa_ostukorv_kasutaja WHERE username='$username' AND password=SHA1('$password') LIMIT 1") or die(mysqli_error($connection));
         $row = mysqli_fetch_array($result);
         $count = mysqli_num_rows($result);
         if ($count == 1) {
